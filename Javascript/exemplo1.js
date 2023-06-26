925fda955cb76f42a80c31e82526d8550b039bf1
Isabely Oliveira <isabelyoliveira.belt1@gmail.com>
2023-06-26 19:29:27 -0300

    adicionado a linha seis

--- Javascript/exemplo1.js
+++ Javascript/exemplo1.js
@@ -1,5 +1,14 @@
 // Numeros { inteiros / decimais} Lógicos (Booleano { true / false}) Textos (Strings) Objetos (arrays) Indefinido (underfined)
 
 
+let nome = prompt(`Informe o seu nome:`); // solicita informações  
+
+let teste = "Jose"
+
+alert(`Bem vindo ${nome}`); // mostra uma informação
+
+let soma = 11 + 5;{
 
 
+
+alert(`${soma}`);
