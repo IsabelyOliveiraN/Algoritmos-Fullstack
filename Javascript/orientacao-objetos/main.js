f5ee62be37444c4ce713f45ac98853d3e8f2c950
Isabely Oliveira <isabelyoliveira.belt1@gmail.com>
2023-07-17 19:52:32 -0300

    orientacao a objetos

---
+++ Javascript/orientacao-objetos/main.js
@@ -0,0 +1,10 @@
+import { Carro } from './carro.js';
+
+
+// instanciamento o objeto up
+const up = new Carro(`Up`, `VW`, 2015);
+
+// instanciamento o objeto Ka
+const Ka = new Carro(`Ka`, `Ford`, 2006);
+
+alert(up.ano);
